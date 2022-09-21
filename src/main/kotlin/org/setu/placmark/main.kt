817f7af68b64e69cf45f45d9f656c706458a48f2
Martin McConnell <20088021@mail.wit.ie>
2022-09-21 07:07:07 +0100

    Added Logging Support

--- src/main/kotlin/org/setu/placmark/main.kt
+++ src/main/kotlin/org/setu/placmark/main.kt
@@ -1,5 +1,9 @@
 package org.setu.placemark
 
+import mu.KotlinLogging
+
+private val logger= KotlinLogging.logger {}
 fun main(args: Array<String>){
+    logger.info { "Launching Placemark Console App" }
     println("Placemark Kotlin App Version 1.0")
 }
