afbf219a5e305791c78b9c428e96a1fcd0b6f829
Martin McConnell <20088021@mail.wit.ie>
2022-09-21 07:12:01 +0100

    Added some menu support

--- src/main/kotlin/org/setu/placmark/main.kt
+++ src/main/kotlin/org/setu/placmark/main.kt
@@ -3,7 +3,37 @@ package org.setu.placemark
 import mu.KotlinLogging
 
 private val logger= KotlinLogging.logger {}
+
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("Main Menu")
+    println(" 1. Add Placemark")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+    return option
+}
 fun main(args: Array<String>){
     logger.info { "Launching Placemark Console App" }
     println("Placemark Kotlin App Version 1.0")
+
+    var input: Int
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("You Chose Add Placemark")
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting Down Placemark Console App" }
 }
